be9c68feda9894dbca3a666fa175c17db545ff30
purofle <purofle@gmail.com>
2024-03-23 23:21:23 +0800

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,72 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
+import {useState} from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [copied, setCopied] = useState(false);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    return (
+        <main className="bg-gray-800 flex flex-col justify-center items-center h-screen p-10 md:p-24">
+            <div className="text-center p-4">
+                <h1 className="text-4xl font-bold text-white">This is <span className="text-pink-300">purofle</span>
+                </h1>
+                <h2 className="text-xl text-gray-400 pt-2 md:pt-5 line-through">我很可爱，请给我钱</h2>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <div className="flex flex-col gap-6">
+                <div className="border border-gray-600 rounded-lg p-8 flex-1 text-center">
+                    <h2 className="text-xl font-semibold text-white">使用虚拟货币</h2>
+                    <p className="text-sm text-gray-300">
+                        使用虚拟货币给我打钱捐赠，点击下面的按钮来复制地址。
+                    </p>
+                    <div className="mt-4 flex justify-center">
+                        <button
+                            className="mt-4 bg-blue-400 hover:bg-blue-600 text-white py-2 px-4 rounded-md mr-4"
+                            onClick={() => {
+                                navigator.clipboard.writeText("TQBAYwveaQZhQPMNGsYWqZDbCda7GuT6iu").then(r => {
+                                    makeCopied();
+                                })
+                            }
+                            }>Tether
+                        </button>
+                        <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md mr-4"
+                                onClick={() => {
+                                    navigator.clipboard.writeText("0x6607A11C4Be2E99cA42FbEdFCAE8F295523aC80e").then(r => {
+                                        makeCopied();
+                                    })
+                                }}>EVM
+                            Chain(BSC,ETH)
+                        </button>
+                        <button className="mt-4 bg-blue-600 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+                                onClick={() => {
+                                    navigator.clipboard.writeText("DH3zGJQTm4yqDujKTSsCxiQgTSMqLoKsQki1mjsuV3tS").then(r => {
+                                        makeCopied();
+                                    })
+                                }}>Solana
+                        </button>
+                    </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+                    <p className={"text-green-500 mt-2 transition-opacity ease-in-out delay-150 duration-300 " + (copied ? "opacity-100" : "opacity-0")}>已复制</p>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                <div className="border border-gray-600 rounded-lg p-8 flex-1 text-center">
+                    <h2 className="text-xl font-semibold text-white">使用 Stripe 捐赠</h2>
+                    <p className="text-sm text-gray-300">
+                        使用 Stripe 捐赠，支持支付宝及银行卡。
+                    </p>
+                    <a href="https://donate.stripe.com/bIY9CAfTh5IweDmfZm"><button className="mt-4 bg-blue-400 hover:bg-blue-600 text-white py-2 px-4 rounded-md">捐赠
+                    </button></a>
+                </div>
+            </div>
+        </main>
+    );
+
+    function makeCopied() {
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 500);
+    }
 }
